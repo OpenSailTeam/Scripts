4015b11498f92d3cfd63aeb7446dad4175809b23
OpenSailTeam <114253205+OpenSailTeam@users.noreply.github.com>
2023-05-31 11:19:01 -0600

    Update UTM-Cookie-Form-Datalayer.js

--- UTM-Cookie-Form-Datalayer.js
+++ UTM-Cookie-Form-Datalayer.js
@@ -132,36 +132,34 @@ window.addEventListener("load", (event) => {
     function populateData(forms, forceSubmit) {
           for (let form of forms) {
               (function() {
-                  form.addEventListener('submit', (event) => {
-                    event.preventDefault(); // prevent page refresh
-                    //console.log("form: " + form);
-                    let formData = new FormData(form);
-                    for (let p of formData) {
+                    form.addEventListener('submit', (event) => {
+                      event.preventDefault(); // prevent page refresh
+                      //console.log("form: " + form);
+                      let formData = new FormData(form);
+                      for (let p of formData) {
                         let pair = {};
                         let key = p[0];
                         let value = p[1];
                         if (key.startsWith('q')) {
-                            const regex = /^q\d+_(.*)$/;
-                            const match = key.match(regex);
-                            if (match) {
-                                key = match[1];
-                            }
+                          const regex = /^q\d+_(.*)$/;
+                          const match = key.match(regex);
+                          if (match) {
+                            key = match[1];
+                          }
                         }
                         pair[key] = value;
                         //console.log(pair);
                         window.dataLayer.push(pair);
-                    }
+                      }
 
-                    let eventId = {};
-                    eventId["event_id"] = Date.now().toString();
-                    window.dataLayer.push(eventId);
-
-                    setTimeout(() => {
-                        if (forceSubmit) {
-                            form.submit();
-                        }
-                    }, 5000); // wait 5 seconds before submitting the form
-                });
+                      let eventId = {};
+                      eventId["event_id"] = Date.now().toString();
+                      window.dataLayer.push(eventId);
+                      
+                      if (forceSubmit) {
+                        form.submit();
+                      }
+                  });
               }());
           }
       }
