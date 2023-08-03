09a43b7dae0be4dd51caee18535583ac8159b00c
OpenSailTeam <114253205+OpenSailTeam@users.noreply.github.com>
2023-08-03 09:41:25 -0600

    Update UTM-Cookie-Form-Datalayer.js

--- UTM-Cookie-Form-Datalayer.js
+++ UTM-Cookie-Form-Datalayer.js
@@ -1,6 +1,8 @@
 window.addEventListener("load", (event) => {
 
     const my_utmParameters = [
+        "gclid",
+        "fbclid",
         "utm_source",
         "utm_medium",
         "utm_campaign"
