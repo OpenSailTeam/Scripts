0d63566a407f1dc3729a985a4c1f82fb2023289e
OpenSailTeam <114253205+OpenSailTeam@users.noreply.github.com>
2023-08-24 12:03:51 -0600

    Update and rename Jotform-Hotfix-Test4.js to Jotform-Hotfix-Test5.js

--- Jotform-Hotfix-Test5.js
+++ Jotform-Hotfix-Test5.js
@@ -145,7 +145,7 @@ window.addEventListener("load", (event) => {
                 let hasEmptyRequiredField = false;
                 for (let element of form.elements) {
                     // Check if the element is nested within a <li> with class 'always-hidden'
-                    if (element.closest('li.always-hidden')) {
+                    if (element.closest('.always-hidden, .form-field-hidden')) {
                         continue; // Skip this element
                     }
                     
